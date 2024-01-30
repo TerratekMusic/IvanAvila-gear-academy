d08a3d2cfe26878b391438b4cf4de6d7e9b56971
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-01-30 12:11:39 -0600

    Update lib.rs

--- contracts/07-coursework-battle/src/lib.rs
+++ contracts/07-coursework-battle/src/lib.rs
@@ -1,27 +1,255 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use gstd::{async_main, fmt, debug, exec, msg, prelude::*, ActorId};
-use sharded_fungible_token_io::FTokenAction;
-use sharded_fungible_token_io::FTokenEvent;
-use sharded_fungible_token_io::LogicAction;
+use gstd::{async_main, debug, exec, fmt, msg, prelude::*, ActorId, ReservationId};
+use sharded_fungible_token_io::{FTokenAction, FTokenEvent, LogicAction};
 use store_io::{StoreAction, StoreEvent};
-use tamagotchi_auto_io::{Tamagotchi, TmgEvent, TmgAction};
-
-
-const HUNGER_PER_BLOCK: u64 = 1;
-const BOREDOM_PER_BLOCK: u64 = 2;
-const ENERGY_PER_BLOCK: u64 = 2;
-const FILL_PER_FEED: u64 = 1000;
-const FILL_PER_ENTERTAINMENT: u64 = 1000;
-const FILL_PER_SLEEP: u64 = 1000;
+use tamagotchi_auto_io::{Tamagotchi, TmgAction, TmgEvent};
 
 static mut TAMAGOTCHI: Option<Tamagotchi> = None;
 
-// TODO: 5️⃣ Add the `approve_tokens` function
+const HUNGER_PER_BLOCK: u32 = 1;
+const HUNGER_THRESHOLD: u32 = 2000;
+const FILL_PER_FEED: u32 = 1000;
+
+const BOREDOM_PER_BLOCK: u32 = 2;
+const BOREDOM_THRESHOLD: u32 = 2000;
+const FILL_PER_ENTERTAINMENT: u32 = 1000;
+
+const ENERGY_PER_BLOCK: u32 = 2;
+const ENERGY_THRESHOLD: u32 = 2000;
+const FILL_PER_SLEEP: u32 = 1000;
+
+const TIME_INTERVAL: u32 = 60;
+const MAX_STAT_VALUE: u32 = 10000;
+
+#[no_mangle]
+extern fn init() {
+    let init_message: String = msg::load().expect("Can't decode tamagotchi's name");
+    let current_block = exec::block_height();
+    let tamagotchi = Tamagotchi {
+        name: init_message,
+        date_of_birth: exec::block_timestamp(),
+        owner: msg::source(),
+        fed: MAX_STAT_VALUE,
+        fed_block: current_block,
+        entertained: MAX_STAT_VALUE,
+        entertained_block: current_block,
+        slept: MAX_STAT_VALUE,
+        slept_block: current_block,
+        approved_account: None,
+        ft_contract_id: Default::default(),
+        transaction_id: Default::default(),
+        approve_transaction: None,
+        reservations: Vec::new(),
+    };
+    debug!(
+        "The Tamagotchi Program was initialized with name {:?} and birth date {:?}",
+        tamagotchi.name, tamagotchi.date_of_birth,
+    );
+
+    msg::send_delayed(exec::program_id(), TmgAction::CheckState, 0, TIME_INTERVAL)
+        .expect("Error in sending initial CheckState action message");
+
+    unsafe { TAMAGOTCHI = Some(tamagotchi) };
+}
+
+#[async_main]
+async fn main() {
+    let tamagotchi = unsafe {
+        TAMAGOTCHI
+            .as_mut()
+            .expect("The contract is not initialized")
+    };
+
+    let tmg_action: TmgAction = msg::load().expect("Error loading TmgAction");
+    match tmg_action {
+        TmgAction::Name => reply_with(&tamagotchi.name),
+        TmgAction::Age => {
+            reply_with((exec::block_timestamp() - tamagotchi.date_of_birth).to_string())
+        }
+        TmgAction::Feed => {
+            fill_stat_and_reply(
+                &mut tamagotchi.fed,
+                &mut tamagotchi.fed_block,
+                HUNGER_PER_BLOCK,
+                FILL_PER_FEED,
+            );
+            debug!("Feed action {:?}", &tamagotchi.fed.to_string());
+        }
+        TmgAction::Entertain => {
+            fill_stat_and_reply(
+                &mut tamagotchi.entertained,
+                &mut tamagotchi.entertained_block,
+                BOREDOM_PER_BLOCK,
+                FILL_PER_ENTERTAINMENT,
+            );
+            debug!("Entertain action {:?}", &tamagotchi.entertained.to_string());
+        }
+        TmgAction::Sleep => {
+            fill_stat_and_reply(
+                &mut tamagotchi.slept,
+                &mut tamagotchi.slept_block,
+                ENERGY_PER_BLOCK,
+                FILL_PER_SLEEP,
+            );
+            debug!("Sleep action {:?}", &tamagotchi.slept.to_string());
+        }
+        TmgAction::Transfer(new_owner) => {
+            if msg::source() == tamagotchi.owner
+                || Some(msg::source()) == tamagotchi.approved_account
+            {
+                tamagotchi.owner = new_owner;
+                msg::reply(TmgEvent::Transferred(new_owner), 0).expect("Error in sending reply");
+            } else {
+                panic!("You don't have permission to do this action")
+            }
+        }
+        TmgAction::Approve(account) => {
+            if msg::source() == tamagotchi.owner {
+                tamagotchi.approved_account = Some(account);
+                msg::reply(TmgEvent::Approved(account), 0).expect("Error in sending reply");
+            } else {
+                panic!("You don't have permission to do this action")
+            }
+        }
+        TmgAction::RevokeApproval => {
+            if msg::source() == tamagotchi.owner {
+                tamagotchi.approved_account = None;
+                msg::reply(TmgEvent::ApprovalRevoked, 0).expect("Error in sending reply");
+            } else {
+                panic!("You don't have permission to do this action")
+            }
+        }
+        TmgAction::SetFTokenContract(contract) => {
+            tamagotchi.ft_contract_id = Some(contract);
+            msg::reply(TmgEvent::FTokenContractSet, 0)
+                .expect("Error in a reply `TmgEvent::FTokenContractSet`");
+        }
+        TmgAction::ApproveTokens { account, amount } => {
+            approve_tokens(tamagotchi, &account, amount).await;
+            msg::reply(TmgEvent::TokensApproved { account, amount }, 0)
+                .expect("Error in a reply `TmgEvent::TokensApproved`");
+        }
+        TmgAction::BuyAttribute {
+            store_id,
+            attribute_id,
+        } => {
+            buy_attribute(&store_id, attribute_id).await;
+            msg::reply(TmgEvent::AttributeBought(attribute_id), 0)
+                .expect("Error in a reply `TmgEvent::AttributeBought`");
+        }
+        TmgAction::CheckState => {
+            update_stats(
+                tamagotchi,
+                tamagotchi.fed_block,
+                tamagotchi.entertained_block,
+                tamagotchi.slept_block,
+            );
+            if tamagotchi.fed <= HUNGER_THRESHOLD {
+                msg::send_from_reservation(
+                    *tamagotchi.reservations.last().unwrap(),
+                    tamagotchi.owner,
+                    TmgEvent::FeedMe,
+                    0,
+                )
+                .expect("Error when sending message to Tamagotchi owner asking to feed it");
+            }
+            if tamagotchi.entertained <= BOREDOM_THRESHOLD {
+                msg::send_from_reservation(
+                    *tamagotchi.reservations.last().unwrap(),
+                    tamagotchi.owner,
+                    TmgEvent::PlayWithMe,
+                    0,
+                )
+                .expect("Error when sending message to Tamagotchi owner asking to entertain it");
+            }
+            if tamagotchi.slept <= ENERGY_THRESHOLD {
+                msg::send_from_reservation(
+                    *tamagotchi.reservations.last().unwrap(),
+                    tamagotchi.owner,
+                    TmgEvent::WantToSleep,
+                    0,
+                )
+                .expect("Error when sending message to Tamagotchi owner asking to sleep it");
+            }
+            msg::send_delayed(exec::program_id(), TmgAction::CheckState, 0, TIME_INTERVAL)
+                .expect("Error in sending subsequent CheckState action message");
+        }
+        TmgAction::ReserveGas {
+            reservation_amount,
+            duration,
+        } => {
+            let reservation_id = ReservationId::reserve(reservation_amount, duration)
+                .expect("Error in reserving gas");
+            tamagotchi.reservations.push(reservation_id);
+            msg::reply(TmgEvent::GasReserved, 0)
+                .expect("Error in replying GasReserved event payload");
+        }
+    }
+}
+
+#[no_mangle]
+extern fn state() {
+    let tamagotchi = unsafe {
+        TAMAGOTCHI
+            .as_mut()
+            .expect("The contract is not initialized")
+    };
+    update_stats(
+        tamagotchi,
+        tamagotchi.fed_block,
+        tamagotchi.entertained_block,
+        tamagotchi.slept_block,
+    );
+    debug!("Sending state: {:?}", tamagotchi);
+    msg::reply(tamagotchi, 0).expect("Failed to share state");
+}
+
+fn reply_with<T: fmt::Display>(value: T) {
+    msg::reply(&value.to_string(), 0).expect("Error in sending reply");
+}
+
+fn fill_stat_and_reply(
+    stat: &mut u32,
+    stat_block: &mut u32,
+    stat_wasted_per_block: u32,
+    fill_per_action: u32,
+) {
+    let actual_value = update_stat(*stat_block, stat_wasted_per_block);
+
+    *stat = fill_stat_value(actual_value, fill_per_action);
+    *stat_block = exec::block_height();
+
+    reply_with(*stat);
+}
+
+fn update_stat(stat_block: u32, stat_wasted_per_block: u32) -> u32 {
+    let stat_lost = (exec::block_height() - stat_block) * stat_wasted_per_block;
+
+    let stat = 10000u32.saturating_sub(stat_lost).saturating_add(1);
+
+    stat
+}
+
+fn fill_stat_value(stat: u32, fill_per_action: u32) -> u32 {
+    let filled_stat = stat + fill_per_action;
+    filled_stat.max(1).min(10000)
+}
+
+fn update_stats(
+    tamagotchi: &mut Tamagotchi,
+    fed_block: u32,
+    entertained_block: u32,
+    slept_block: u32,
+) {
+    tamagotchi.fed = update_stat(fed_block, HUNGER_PER_BLOCK);
+    tamagotchi.entertained = update_stat(entertained_block, BOREDOM_PER_BLOCK);
+    tamagotchi.slept = update_stat(slept_block, ENERGY_PER_BLOCK);
+}
+
 async fn approve_tokens(tamagotchi: &mut Tamagotchi, account: &ActorId, amount: u128) {
-    // ...
-    let _approve_FT = msg::send_for_reply_as::<_, FTokenEvent>(
+    let _result_approve = msg::send_for_reply_as::<_, FTokenEvent>(
         tamagotchi.ft_contract_id.unwrap(),
         FTokenAction::Message {
             transaction_id: tamagotchi.transaction_id,
@@ -35,7 +263,6 @@ async fn approve_tokens(tamagotchi: &mut Tamagotchi, account: &ActorId, amount: 
     )
     .expect("Error in sending a message `FTokenAction::Message`")
     .await;
-    // ...
 }
 
 async fn buy_attribute(store: &ActorId, attribute: u32) {
@@ -49,157 +276,4 @@ async fn buy_attribute(store: &ActorId, attribute: u32) {
     )
     .expect("Error in sending a message `StoreAction::BuyAttribute`")
     .await;
-}
-
-
-
-#[no_mangle]
-extern fn init() {
-    // TODO: 0️⃣ Copy the `init` function from the previous lesson and push changes to the master branch
-   
-    let tamagotchi = Tamagotchi {
-        name:  msg::load().expect("Can't decode an init message"),
-        date_of_birth: exec::block_timestamp(),
-        owner: msg::source(),
-        fed: 1000,
-        fed_block: exec::block_height().into(),
-        entertained: 1000,
-        entertained_block: exec::block_height().into(),
-        slept: 1000,
-        slept_block: exec::block_height().into(),
-        approved_account: None,
-        ft_contract_id: Default::default(),
-        transaction_id: Default::default(),
-        approve_transaction: None,
-    };
-    debug!(
-        "The Tamagotchi Program was initialized with name {:?}, birth date {:?}, owner: {:?}",
-        tamagotchi.name, tamagotchi.date_of_birth, tamagotchi.owner
-    );
-    unsafe { TAMAGOTCHI = Some(tamagotchi) };
-
-}
-
-#[async_main]
-async fn main() {
-    // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
-    let _tamagotchi = unsafe {
-        TAMAGOTCHI
-            .as_mut()
-            .expect("The contract is not initialized")
-    };
-    
-    let name = &_tamagotchi.name;
-    let current_time = exec::block_timestamp();
-    let age = current_time.saturating_sub(_tamagotchi.date_of_birth);
-    let action: TmgAction = msg::load().expect("Can't decode an action message");
-    
-    // 
-
-  match action {
-        TmgAction::Name => {
-            msg::reply(TmgEvent::Name(name.to_string()), 0).expect("Error in sending name");
-        }
-        TmgAction::Age =>{
-            msg::reply(TmgEvent::Age(age), 0).expect("Error in sending age");
-           
-        } 
-        TmgAction::Feed => {
-            let fed: u64 = _tamagotchi.fed;
-            let fed_block: u64 = _tamagotchi.fed_block;
-            let current_block: u64 = exec::block_height().into();
-            let time_passed: u64 = current_block - fed_block;
-            let hunger: u64 = time_passed * HUNGER_PER_BLOCK;
-            let current_fed:u64  = fed - hunger;
-            let new_fed:u64 = current_fed + FILL_PER_FEED;
-           
-            let new_fed_block: u64 = current_block;
-            _tamagotchi.fed = new_fed;
-            _tamagotchi.fed_block = new_fed_block;
-            msg::reply(TmgEvent::Fed(new_fed), 0).expect("Error in sending fed");
-        }
-        TmgAction::Entertain => {
-            let entertained: u64 = _tamagotchi.entertained;
-           let entertained_block: u64 = _tamagotchi.entertained_block;
-            let current_block: u64 = exec::block_height().into();
-            let time_passed: u64 = current_block - entertained_block;
-            let boredom: u64 = time_passed * BOREDOM_PER_BLOCK;
-            let current_entertained: u64 = entertained - boredom;
-            let new_entertained: u64 = current_entertained + FILL_PER_ENTERTAINMENT;
-
-            let new_entertained_block: u64 = current_block;
-            _tamagotchi.entertained = new_entertained;
-            _tamagotchi.entertained_block = new_entertained_block;
-            msg::reply(TmgEvent::Entertained(new_entertained), 0).expect("Error in sending entertained");
-            
-        }
-        TmgAction::Sleep => {
-            let slept: u64 = _tamagotchi.slept;
-            let slept_block: u64 = _tamagotchi.slept_block;
-            let current_block: u64 = exec::block_height().into();
-            let time_passed: u64 = current_block - slept_block;
-            let energy: u64 = time_passed * ENERGY_PER_BLOCK;
-            let current_slept: u64 = slept - energy;
-            let new_slept: u64 = current_slept + FILL_PER_SLEEP;
-
-            let new_slept_block: u64 = current_block;
-            _tamagotchi.slept = new_slept;
-            _tamagotchi.slept_block = new_slept_block;
-            msg::reply(TmgEvent::Slept(new_slept), 0).expect("Error in sending slept");
-        
-        }
-        TmgAction::Transfer(new_owner) => {
-            if _tamagotchi.owner == msg::source() {
-                _tamagotchi.owner = new_owner;
-                msg::reply(TmgEvent::Transfer(new_owner), 0).expect("Error in sending transfer");
-                // debug!("Tamagotchi Transfered to account: {:?}", new_owner)
-            }else{
-                panic!("You are not the owner of this Tamagotchi");}
-         
-        }
-        TmgAction::Approve(account) => {
-            if _tamagotchi.owner == msg::source() {
-            _tamagotchi.approved_account = Some(account);
-            msg::reply(TmgEvent::Approve(account), 0).expect("Error in sending approve");
-            // debug!("Approved account: {:?}", account)
-            }else{
-                panic!("You are not the allowed to approve accounts for this Tamagotchi");}
-        }
-        TmgAction::RevokeApproval => {
-            if _tamagotchi.owner == msg::source() {
-            _tamagotchi.approved_account = None;
-            msg::reply(TmgEvent::RevokeApproval, 0).expect("Error in sending revoke approval");
-            // debug!("Approved account: {:?} has been revoked", _tamagotchi.approved_account)
-            }else{
-                panic!("You are not the allowed to revoke approval for this Tamagotchi");}
-            }
-            // TODO; 6️⃣ Add handling new actions
-        TmgAction::SetFTokenContract(contract) => {
-            _tamagotchi.ft_contract_id = Some(contract);
-            msg::reply(TmgEvent::FTokenContractSet, 0)
-                .expect("Error in a reply `TmgEvent::FTokenContractSet`");
-        }
-        TmgAction::ApproveTokens { account, amount } => {
-            approve_tokens(_tamagotchi, &account, amount).await;
-            msg::reply(TmgEvent::TokensApproved { account, amount }, 0)
-                .expect("Error in a reply `TmgEvent::TokensApproved`");
-        }
-        TmgAction::BuyAttribute { store_id, attribute_id } => {
-            buy_attribute(&store_id, attribute_id).await;
-            msg::reply(TmgEvent::AttributeBought(attribute_id), 0)
-                .expect("Error in a reply `TmgEvent::AttributeBought`");
-        }
-    };
-
-}
-
-#[no_mangle]
-extern fn state() {
-    // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
-    let tamagotchi = unsafe {
-        TAMAGOTCHI
-            .as_ref()
-            .expect("The contract is not initialized")
-    };
-    msg::reply(tamagotchi, 0).expect("Failed to share state");
 }
