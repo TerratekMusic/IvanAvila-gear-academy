c16808bcdfd127b8f76232f84e02472f1afc6ea6
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-01-31 23:08:21 -0600

    Luchex Update

--- contracts/07-coursework-battle/battle/src/lib.rs
+++ contracts/07-coursework-battle/battle/src/lib.rs
@@ -1,16 +1,17 @@
 
 #![no_std]
-use battle_io::{BattleAction, BattleEvent, BattleState};
-use gstd::{debug, exec, msg, prelude::*, ActorId, panic, collections::{BTreeMap, BTreeSet}};
-use coursework_battle_io::{ TmgAction, TmgEvent};
-use store_io::{StoreAction, StoreEvent, AttributeId, TamagotchiId};
-
+use gmeta::{In, InOut, Metadata, Out};
+use gstd::{collections::BTreeSet, exec, msg, panic, prelude::*, ActorId};
+use store_io::{AttributeId, StoreAction, StoreEvent};
+use coursework_battle_io::{TmgAction, TmgEvent};
 
 const MAX_POWER: u16 = 10_000;
 const MIN_POWER: u16 = 3_000;
 
+const MAX_ENERGY: u16 = 20_000;
+const MIN_ENERGY: u16 = 6_000;
+
 const SWORD_POWER: u16 = 2;
-const SWORD_ID: AttributeId = Default::default();
 
 //CONST SHIELD_POWER: u16 = 2;
 //CONST SHIELD_ID: AttributeId = Default::default();
@@ -19,30 +20,33 @@ const TIME_FOR_UPDATE: u32 = 500;
 const MAX_STEPS_FOR_ROUND: u8 = 3;
 const GAS_AMOUNT: u64 = 100_000_000;
 
-static mut BATTLE : Option<Battle> = None;
-static mut PLAYER : Option<Player> = None;
+pub type TamagotchiId = ActorId;
 
-#[derive(Default)]
-struct Battle {
-    players: Vec<Player>,
-    state: BattleState,
-    current_turn: u8,
-    tmg_store_id: ActorId,
-    winner: ActorId,
-    steps: u8,
+#[derive(Default, Encode, Decode, TypeInfo, PartialEq, Eq, Debug)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum BattleState {
+    #[default]
+    Registration,
+    Moves,
+    Waiting,
+    GameIsOver,
 }
 
-#[derive(Default, Clone)]
-pub struct Player {
-    owner: ActorId,
-    tmg_id: TamagotchiId,
-    energy: u16,
-    power: u16,
-    attributes: BTreeSet<AttributeId>,
+#[derive(Default, Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct Battle {
+    pub players: Vec<Player>,
+    pub state: BattleState,
+    pub current_turn: u8,
+    pub tmg_store_id: ActorId,
+    pub winner: ActorId,
+    pub steps: u8,
 }
 
 impl Battle {
-    async fn register(&mut self, tmg_id: &TamagotchiId) {
+    pub async fn register(&mut self, tmg_id: &TamagotchiId) {
         assert_eq!(
             self.state,
             BattleState::Registration,
@@ -54,6 +58,7 @@ impl Battle {
 
         let power = generate_power();
         let power = MAX_POWER - power;
+        let energy = generate_energy();
         let player = Player {
             owner,
             tmg_id: *tmg_id,
@@ -70,25 +75,21 @@ impl Battle {
             .expect("Error during a reply `BattleEvent::Registered");
     }
 
-    
-
     pub fn get_turn() -> u8 {
         let random_input: [u8; 32] = array::from_fn(|i| i as u8 + 1);
-        let (random, _) = exec::random(random_input)
-            .expect("Error in getting random number");
+        let (random, _) = exec::random(random_input).expect("Error in getting random number");
         random[0] % 2
     }
 
-    
-    fn make_move(&mut self) {
+    pub fn make_move(&mut self) {
         assert_eq!(
             self.state,
             BattleState::Moves,
             "The game is not in `Moves` state"
         );
         let turn = self.current_turn as usize;
-    
-        let next_turn = (( turn + 1 ) % 2)as usize;
+
+        let next_turn = ((turn + 1) % 2) as usize;
         let player = self.players[turn].clone();
         assert_eq!(
             player.owner,
@@ -96,28 +97,29 @@ impl Battle {
             "You are not in the game or it is not your turn"
         );
         let mut opponent = self.players[next_turn].clone();
-        let sword_power = if player.attributes.contains(&SWORD_ID) {
-            SWORD_POWER
-        } else {
-            1
-        };
-    
+        // let sword_power = if player.attributes.contains(&SWORD_ID) {
+        //     SWORD_POWER
+        // } else {
+        //     1
+        // };
+        let sword_power = SWORD_POWER;
+
         opponent.energy = opponent.energy.saturating_sub(sword_power * player.power);
-    
+
         self.players[next_turn] = opponent.clone();
         // Check if the opponent lost
         if opponent.energy == 0 {
             self.players = Vec::new();
             self.state = BattleState::GameIsOver;
             self.winner = player.tmg_id;
-            msg::reply(BattleEvent::GameOver(self.winner), 0)
+            msg::reply(BattleEvent::GameIsOver, 0)
                 .expect("Error in sending a reply `BattleEvent::GameIsOver`");
             return;
         }
         if self.steps <= MAX_STEPS_FOR_ROUND {
             self.steps += 1;
             self.current_turn = next_turn as u8;
-            msg::reply(BattleEvent::Moved, 0)
+            msg::reply(BattleEvent::MoveMade, 0)
                 .expect("Error in sending a reply `BattleEvent::MoveMade`");
         } else {
             self.state = BattleState::Waiting;
@@ -134,7 +136,7 @@ impl Battle {
                 .expect("Error in sending a reply `BattleEvent::MoveMade`");
         }
     }
-    async fn update_info(&mut self) {
+    pub async fn update_info(&mut self) {
         assert_eq!(
             msg::source(),
             exec::program_id(),
@@ -145,7 +147,7 @@ impl Battle {
             BattleState::Waiting,
             "The contract must be in `Waiting` state"
         );
-    
+
         for i in 0..2 {
             let player = &mut self.players[i];
             let attributes = get_attributes(&self.tmg_store_id, &player.tmg_id).await;
@@ -158,43 +160,56 @@ impl Battle {
     }
 }
 
-
-#[no_mangle]
-extern fn init() {
-    let init_message: BattleAction = msg::load().expect("Can't decode regitration message");
-    let battle = Battle{
-        players: Vec::new(),
-        state: BattleState::Registration,
-        current_turn: 0,
-        tmg_store_id: ActorId::default(),
-        winner: ActorId::default(),
-        steps: 0,
-    };
-
-  unsafe{
-    BATTLE = Some(battle);
-  }
-    
+#[derive(Default, Encode, Decode, TypeInfo, Clone)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct Player {
+    pub owner: ActorId,
+    pub tmg_id: TamagotchiId,
+    pub energy: u16,
+    pub power: u16,
+    pub attributes: BTreeSet<AttributeId>,
 }
 
-#[gstd::async_main]
-async fn main() {
+#[derive(Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum BattleAction {
+    Register(TamagotchiId),
+    Move,
+    UpdateInfo,
+    // Attack(TamagotchiId, u8),
+    // Defend(TamagotchiId, u8),
+    // Surrender(TamagotchiId),
+}
 
+#[derive(Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum BattleEvent {
+    Registered { tmg_id: TamagotchiId },
+    Moved { tmg_id: TamagotchiId, steps: u8 },
+    GameOver { winner: ActorId },
+    GoToWaitingState,
+    InfoUpdated,
+    GameIsOver,
+    MoveMade,
+}
 
-    let action: BattleAction = msg::load().expect("Unable to decode `BattleAction`");
-    let battle: &mut Battle = unsafe { BATTLE.get_or_insert(Default::default()) };
+pub struct ProgramMetadata;
 
-    match action {
-        BattleAction::UpdateInfo => battle.update_info().await,
-        BattleAction::Move => battle.make_move().await,
-        BattleAction::Register (TamagotchiId) => battle.register(&TamagotchiId).await,
-    }
-
+impl Metadata for ProgramMetadata {
+    type Init = In<ActorId>;
+    type Handle = InOut<BattleAction, BattleEvent>;
+    type Reply = ();
+    type Others = ();
+    type Signal = ();
+    type State = Out<Battle>;
 }
 
 pub async fn get_owner(tmg_id: &ActorId) -> ActorId {
     let reply: TmgEvent = msg::send_for_reply_as(*tmg_id, TmgAction::TmgInfo, 0, 0)
-        .expect("Error in sending a message `TmgAction::TmgInfo`")
+        .expect("Error in sending a message `TmgAction::Owner")
         .await
         .expect("Unable to decode TmgEvent");
     if let TmgEvent::Owner(owner) = reply {
@@ -204,11 +219,9 @@ pub async fn get_owner(tmg_id: &ActorId) -> ActorId {
     }
 }
 
-
 pub fn generate_power() -> u16 {
     let random_input: [u8; 32] = array::from_fn(|i| i as u8 + 1);
-    let (random, _) = exec::random(random_input)
-        .expect("Error in getting random number");
+    let (random, _) = exec::random(random_input).expect("Error in getting random number");
     let bytes: [u8; 2] = [random[0], random[1]];
     let random_power: u16 = u16::from_be_bytes(bytes) % MAX_POWER;
     if random_power < MIN_POWER {
@@ -217,12 +230,18 @@ pub fn generate_power() -> u16 {
     random_power
 }
 
+pub fn generate_energy() -> u16 {
+    let random_input: [u8; 32] = array::from_fn(|i| i as u8 + 1);
+    let (random, _) = exec::random(random_input).expect("Error in getting random number");
+    let bytes: [u8; 2] = [random[0], random[1]];
+    let random_energy: u16 = u16::from_be_bytes(bytes) % MAX_ENERGY;
+    if random_energy < MIN_ENERGY {
+        return MAX_ENERGY / 2;
+    }
+    random_energy
+}
 
-
-async fn get_attributes(
-    tmg_store_id: &ActorId,
-    tmg_id: &TamagotchiId,
-) -> BTreeSet<AttributeId> {
+async fn get_attributes(tmg_store_id: &ActorId, tmg_id: &TamagotchiId) -> BTreeSet<AttributeId> {
     let reply: StoreEvent = msg::send_for_reply_as(
         *tmg_store_id,
         StoreAction::GetAttributes {
@@ -243,12 +262,6 @@ async fn get_attributes(
 
 pub fn get_turn() -> u8 {
     let random_input: [u8; 32] = array::from_fn(|i| i as u8 + 1);
-    let (random, _) = exec::random(random_input)
-        .expect("Error in getting random number");
+    let (random, _) = exec::random(random_input).expect("Error in getting random number");
     random[0] % 2
 }
-
-
-
-
-
